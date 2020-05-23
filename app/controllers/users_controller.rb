f81f28f73d04c2ccaa9cea88e1c3918a6b79d124
s.o <yzk612@gmail.com>
2020-05-23 09:19:03 +0000

    fix userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,7 +18,8 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = 'ユーザを登録しました。'
-      redirect_to root_url
+      redirect_to @user
+      
     else
       flash.now[:danger] = 'ユーザの登録に失敗しました。'
       render :new
